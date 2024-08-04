eb73f504289e7b7ff4fac7b1e181647198d2fa3b
zuk101777 <zuk101777@gmail.com>
2024-08-03 20:20:21 -0400

    added navigation to project, loginfragment, startupfragment, and naviagation build files

--- app/src/main/java/khan/z/dermagazeai/MainActivity.kt
+++ app/src/main/java/khan/z/dermagazeai/MainActivity.kt
@@ -5,16 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                // R.id.confirmationFragment,  R.id.signupFragment
+                R.id.startupFragment, R.id.loginFragment,  -> supportActionBar?.hide()
+                else -> {
+                    supportActionBar?.show()
+                    setupActionBarWithNavController(navController, appBarConfiguration)
+                }
+            }
         }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/khan/z/dermagazeai/registration/StartupFragment.kt
@@ -0,0 +1,34 @@
+package khan.z.dermagazeai.registration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import khan.z.dermagazeai.R
+
+class StartupFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_startup, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val buttonLogin: View = view.findViewById(R.id.buttonLogin)
+        //val buttonSignup: View = view.findViewById(R.id.buttonSignup)
+
+        buttonLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_startupFragment_to_loginFragment)
+        }
+
+//        buttonSignup.setOnClickListener {
+//            findNavController().navigate(R.id.action_startupFragment_to_signupFragment)
+//        }
+    }
+}
