e90502bc851bbb0ad76319fe2bdc50202bdb68fe
zuk101777 <zuk101777@gmail.com>
2024-08-04 20:30:56 -0400

    added error handler for google sign in

--- app/src/main/java/khan/z/dermagazeai/registration/helpers/FacebookSignInHandler.kt
+++ app/src/main/java/khan/z/dermagazeai/registration/helpers/FacebookSignInHandler.kt
@@ -20,63 +20,6 @@ import com.facebook.login.widget.LoginButton
 import khan.z.dermagazeai.R
 import khan.z.dermagazeai.registration.SignInMethod
 
-//class FacebookSignInHandler(private val fragment: Fragment, private val navController: NavController) {
-//
-//    private val callbackManager: CallbackManager = CallbackManager.Factory.create()
-//    private lateinit var loginButton: LoginButton
-//
-//    fun initializeFacebookLogin(view: View, loginButtonId: Int) {
-//        loginButton = view.findViewById(loginButtonId)
-//        loginButton.setPermissions("email")
-//        loginButton.setFragment(fragment)
-//
-//        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-//            override fun onSuccess(result: LoginResult) {
-//                Log.d("FacebookSignInHandler", "Facebook login successful")
-//                Toast.makeText(fragment.context, "Signed in using Facebook", Toast.LENGTH_SHORT).show()
-//                AuthorizationUtils.storeSignInMethod(fragment.requireActivity(), SignInMethod.FACEBOOK)
-//                handleFacebookAccessToken(result.accessToken)
-//            }
-//            override fun onCancel() {
-//                Log.d("FacebookSignInHandler", "Facebook login cancelled")
-//            }
-//            override fun onError(error: FacebookException) {
-//                Log.e("FacebookSignInHandler", "Facebook login error", error)
-//            }
-//        })
-//    }
-//
-//    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        callbackManager.onActivityResult(requestCode, resultCode, data)
-//    }
-//
-//    private fun handleFacebookAccessToken(token: AccessToken) {
-//        Log.d("FacebookSignInHandler", "Handling Facebook access token")
-//        Amplify.Auth.signInWithSocialWebUI(
-//            AuthProvider.facebook(),
-//            fragment.requireActivity(),
-//            { result ->
-//                fragment.requireActivity().runOnUiThread {
-//                    Log.d("FacebookSignInHandler", "Sign-in result: ${result.isSignedIn}")
-//                    if (result.isSignedIn) {
-//                        // Successfully signed in
-//                        Log.d("FacebookSignInHandler", "User is signed in with Facebook")
-//                        navController.navigate(R.id.action_loginFragment_to_homeFragment)
-//                    } else {
-//                        Log.w("FacebookSignInHandler", "Sign-in result was not signed in")
-//                    }
-//                }
-//            },
-//            { error ->
-//                fragment.requireActivity().runOnUiThread {
-//                    Log.e("AmplifyQuickstart", "Sign-in failed", error)
-//                }
-//            }
-//        )
-//    }
-//}
-
-
 class FacebookSignInHandler(private val fragment: Fragment, private val navController: NavController) {
 
     private val callbackManager: CallbackManager = CallbackManager.Factory.create()
--- app/src/main/java/khan/z/dermagazeai/registration/helpers/GoogleSignInHandler.kt
+++ app/src/main/java/khan/z/dermagazeai/registration/helpers/GoogleSignInHandler.kt
@@ -10,83 +10,18 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
-import com.amplifyframework.api.ApiException
 import com.amplifyframework.auth.AuthProvider
 import com.amplifyframework.core.Amplify
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes
 import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import khan.z.dermagazeai.R
 import khan.z.dermagazeai.registration.SignInMethod
-
-//class GoogleSignInHandler(private val fragment: Fragment, private val navController: NavController) {
-//
-//    private lateinit var googleSignInClient: GoogleSignInClient
-//
-//    companion object {
-//        private const val RC_SIGN_IN = 9001
-//    }
-//
-//    fun initializeGoogleSignIn(googleAppId: String) {
-//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestEmail()
-//            .requestIdToken(googleAppId)
-//            .build()
-//
-//        googleSignInClient = GoogleSignIn.getClient(fragment.requireActivity(), gso)
-//    }
-//
-//    fun signIn() {
-//        val signInIntent = googleSignInClient.signInIntent
-//        fragment.startActivityForResult(signInIntent, RC_SIGN_IN)
-//    }
-//
-//    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        if (requestCode == RC_SIGN_IN) {
-//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-//            handleSignInResult(task)
-//        }
-//    }
-//
-//    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-//        try {
-//            val account = completedTask.getResult(com.amplifyframework.api.ApiException::class.java)
-//            Toast.makeText(fragment.context, "Signed in using Google", Toast.LENGTH_SHORT).show()
-//            AuthorizationUtils.storeSignInMethod(fragment.requireActivity(), SignInMethod.GOOGLE)
-//            Log.d("GoogleSignInHandler", "Google sign-in successful")
-//            handleGoogleSignInAccount(account)
-//        } catch (e: com.amplifyframework.api.ApiException) {
-//            Log.e("GoogleSignInHandler", "Google sign-in failed", e)
-//        }
-//    }
-//
-//    private fun handleGoogleSignInAccount(account: GoogleSignInAccount?) {
-//        Log.d("GoogleSignInHandler", "Handling Google sign-in account")
-//        account?.let { googleAccount ->
-//            val idToken = googleAccount.idToken
-//            if (idToken != null) {
-//                Amplify.Auth.signInWithSocialWebUI(
-//                    AuthProvider.google(),
-//                    fragment.requireActivity(),
-//                    { result ->
-//                        Log.i("AuthQuickStart", "Federated google Successfully: $result")
-//                        fragment.requireActivity().runOnUiThread {
-//                            navController.navigate(R.id.action_loginFragment_to_homeFragment)
-//                        }
-//                    },
-//                    { error ->
-//                        Log.e("AuthQuickStart", "Federation failed", error)
-//                    }
-//                )
-//            } else {
-//                Log.e("GoogleSignInHandler", "Google Sign-In successful, but no ID token available")
-//            }
-//        }
-//    }
-//}
 
 class GoogleSignInHandler(private val fragment: Fragment, private val navController: NavController) {
 
@@ -127,13 +62,25 @@ class GoogleSignInHandler(private val fragment: Fragment, private val navControl
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
-            val account = completedTask.getResult(com.amplifyframework.api.ApiException::class.java)
+            val account = completedTask.getResult(ApiException::class.java)
             Toast.makeText(fragment.context, "Signed in using Google", Toast.LENGTH_SHORT).show()
             AuthorizationUtils.storeSignInMethod(fragment.requireActivity(), SignInMethod.GOOGLE)
             Log.d("GoogleSignInHandler", "Google sign-in successful")
             handleGoogleSignInAccount(account)
-        } catch (e: com.amplifyframework.api.ApiException) {
+        } catch (e: ApiException) {
             Log.e("GoogleSignInHandler", "Google sign-in failed", e)
+            handleSignInFailure(e)
+        }
+    }
+
+    private fun handleSignInFailure(e: ApiException) {
+        when (e.statusCode) {
+            GoogleSignInStatusCodes.SIGN_IN_CANCELLED -> {
+                Toast.makeText(fragment.context, "Sign-in cancelled", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Toast.makeText(fragment.context, "Sign-in failed: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
